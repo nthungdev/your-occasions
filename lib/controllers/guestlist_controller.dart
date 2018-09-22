7ccbea979e333b21fb4d2b88c8bd47d5ecafd6ab
Jake Le <lduon002@plattsburgh.ntds>
2018-09-22 17:33:46 -0400

    Controller typo fix

--- lib/controllers/guestlist_controller.dart
+++ lib/controllers/guestlist_controller.dart
@@ -17,27 +17,27 @@ class GuestListController extends BaseController{
   
 
   // METHODS //
-  /// Insert a new row into event_ratings table.
+  /// Insert a new row into guestlists table.
   Future<void> insert(GuestList model) async {
     await connect();
 
-    await connection.query("""INSERT INTO event_ratings (user_id, event_id, creation)_date)
+    await connection.query("""INSERT INTO guestlists (user_id, event_id, creation)_date)
       VALUES (@userId, @eventId, @creationDate)""",
       substitutionValues: model.getProperties());
 
     await disconnect();
   }
 
-  /// Delete an existing row from event_ratings table.
+  /// Delete an existing row from guestlists table.
   Future<void> delete(int id) async {
     await connect();
 
-    await connection.query("""DELETE FROM event_ratings WHERE id = @id""", substitutionValues: { 'id': id, });  
+    await connection.query("""DELETE FROM guestlists WHERE id = @id""", substitutionValues: { 'id': id, });  
 
     await disconnect();
   }
 
-  /// Update an existing row from event_ratings table.
+  /// Update an existing row from guestlists table.
   Future<void> update(int id, {int, userId, int eventId, DateTime creationDate}) async {
     if(userId == null && eventId == null && creationDate == null) {
       throw UpdateQueryException(); //
@@ -45,7 +45,7 @@ class GuestListController extends BaseController{
     else {
       await connect();
 
-      String query = "UPDATE event_ratings SET ";
+      String query = "UPDATE guestlists SET ";
       if(userId != null) { query += "user_id = $userId"; }
       if(eventId != null) { query += "event_id = $eventId "; }
       if(creationDate != null) { query += "creation_date = '$creationDate' "; }
@@ -64,7 +64,7 @@ class GuestListController extends BaseController{
     List<GuestList> result = [];
 
 
-    String query = "SELECT * from event_ratings ";
+    String query = "SELECT * from guestlists ";
 
     if(eventId == null && id == null) {
 
--- lib/controllers/user_attended_controller.dart
+++ lib/controllers/user_attended_controller.dart
@@ -16,27 +16,27 @@ class UserAttendedController extends BaseController{
   
 
   // METHODS //
-  /// Insert a new row into event_ratings table.
+  /// Insert a new row into user_attended_events table.
   Future<void> insert(UserAttendedEvent model) async {
     await connect();
 
-    await connection.query("""INSERT INTO event_ratings (user_id, event_id, creation)_date)
+    await connection.query("""INSERT INTO user_attended_events (user_id, event_id, creation)_date)
       VALUES (@userId, @eventId, @creationDate)""",
       substitutionValues: model.getProperties());
 
     await disconnect();
   }
 
-  /// Delete an existing row from event_ratings table.
+  /// Delete an existing row from user_attended_events table.
   Future<void> delete(int id) async {
     await connect();
 
-    await connection.query("""DELETE FROM event_ratings WHERE id = @id""", substitutionValues: { 'id': id, });  
+    await connection.query("""DELETE FROM user_attended_events WHERE id = @id""", substitutionValues: { 'id': id, });  
 
     await disconnect();
   }
 
-  /// Update an existing row from event_ratings table.
+  /// Update an existing row from user_attended_events table.
   Future<void> update(int id, {int, userId, int eventId, DateTime creationDate}) async {
     if(userId == null && eventId == null && creationDate == null) {
       throw UpdateQueryException(); //
@@ -44,7 +44,7 @@ class UserAttendedController extends BaseController{
     else {
       await connect();
 
-      String query = "UPDATE event_ratings SET ";
+      String query = "UPDATE user_attended_events SET ";
       if(userId != null) { query += "user_id = $userId"; }
       if(eventId != null) { query += "event_id = $eventId "; }
       if(creationDate != null) { query += "creation_date = '$creationDate' "; }
@@ -63,7 +63,7 @@ class UserAttendedController extends BaseController{
     List<UserAttendedEvent> result = [];
 
 
-    String query = "SELECT * from event_ratings ";
+    String query = "SELECT * from user_attended_events ";
 
     if(eventId == null && id == null) {
 
