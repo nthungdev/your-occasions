0258c75433578c9a9c8e7cd92ae1e2e35c80c24c
Hung Nguyen <nthungdev@gmail.com>
2018-09-22 17:44:26 -0400

    update event, event review, event review controller

---
+++ lib/models/event_review.dart
@@ -0,0 +1,68 @@
+class EventReview {
+  // PROPERTIES //
+  int _id;
+  int _eventId;
+  int _reviewerId;
+  int _hostId;
+  int _score;
+  String _description;
+  bool _isUsed;
+  DateTime _creationDate;
+
+  // CONSTRUCTORS //
+  EventReview.create();
+  EventReview({int eventId, int reviewerId, int hostId, String description, bool isUsed}) {
+    eventId = eventId;
+    reviewerId = reviewerId;
+    hostId = hostId;
+    description = description;
+    isUsed = isUsed;
+    creationDate = DateTime.now();
+  }
+  EventReview.createFromMap(Iterable<Map<String, dynamic>> map){
+    map.forEach((item) {
+      id = item['id'];
+      eventId = item['eventId'];
+      reviewerId = item['reviewId'];
+      hostId = item['host_id'];
+      description = item['description'];
+      isUsed = item['is_used'];
+      creationDate = item['creation_date'];
+    });
+  }
+
+  // GETTERS //
+  int get id => _id;
+  int get eventId => _eventId;
+  int get reviewerId => _reviewerId;
+  int get hostId => _hostId;
+  String get description => _description;
+  bool get isUsed => _isUsed;
+  DateTime get creationDate => _creationDate;
+
+  // SETTERS //
+  set id(int id) => _id = id;
+  set eventId(int eventId) => _eventId = eventId;
+  set reviewerId(int reviewerId) => reviewerId = reviewerId;
+  set hostId(int hostId) => _hostId = hostId;
+  set description(String description) => _description = description;
+  set isUsed(bool isUsed) => _isUsed = isUsed;
+  set creationDate(DateTime creationDate) => _creationDate = creationDate;
+
+  // METHODS //
+  /// Return a Map<String, dynamic> with keys are the properties of User, values are the properties' values.
+  Map<String, dynamic> getProperties() {
+    Map<String, dynamic> map = {};
+    map['id'] = id;
+    map['eventId'] = eventId;
+    map['reviewerId'] = reviewerId;
+    map['hostId'] = hostId;
+    map['description'] = description;
+    map['isUsed'] = isUsed;
+    map['creationDate'] = creationDate;
+    return map;
+  }
+
+  String toString() => "An instance of EventReview id=$id.";
+
+}
