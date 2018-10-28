cee5ca714b94e53f2066733ae6dc90ea02a0675d
Jake Le <lethaiduong98@gmail.com>
2018-10-28 16:43:03 -0400

    create event

--- lib/screens/home/drawer.dart
+++ lib/screens/home/drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import './other_page.dart';
 
+import 'package:youroccasions/screens/event/create_event.dart';
 import 'package:youroccasions/screens/login/login.dart';
 import 'package:youroccasions/utilities/config.dart';
 
@@ -25,30 +26,34 @@ class HomeDrawer extends StatelessWidget {
               currentAccountPicture: CircleAvatar(),
             ),
             ListTile (
+              title: Text (" Create Events"),
+              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => CreateEventScreen())),
+                ),
+            ListTile (
               title: Text (" My Events"),
               onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new OtherPage(" My Events"))),
                 ),
-              ListTile (
-                title: Text (" Shared Events"),
-                onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new OtherPage(" Shared Events"))),
-              ),   
+            ListTile (
+              title: Text (" Shared Events"),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new OtherPage(" Shared Events"))),
+            ),   
             ListTile (
                 title: Text (" Location"),
                 onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new OtherPage(" Location"))),
               ),   
-              ListTile (
-                title: Text (" Settings"),
-                onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new OtherPage(" Settings"))),
-              ),   
-              Container(
-              alignment: Alignment.center,
-              child:  MaterialButton(
-                color: Colors.red,
-                onPressed: () async {
-                  await setIsLogin(false);
-                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginWithEmailScreen()));
-                },
-                child: Text("Logout"),
+            ListTile (
+              title: Text (" Settings"),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new OtherPage(" Settings"))),
+            ),   
+            Container(
+            alignment: Alignment.center,
+            child:  MaterialButton(
+              color: Colors.red,
+              onPressed: () async {
+                await setIsLogin(false);
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginWithEmailScreen()));
+              },
+              child: Text("Logout"),
               )
             ),
           ],
--- lib/screens/user/user_profile.dart
+++ lib/screens/user/user_profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:youroccasions/screens/user/diagonally_cut_colored_image.dart';
 import 'package:youroccasions/screens/home/home.dart';
 import 'package:youroccasions/utilities/config.dart';
 import 'package:youroccasions/models/user.dart';
@@ -9,9 +10,9 @@ import 'package:youroccasions/controllers/user_controller.dart';
 final UserController _userController = UserController();
 
 class UserProfileScreen extends StatefulWidget {
-  final user user;
+  final User user;
 
-  UserProfileScreen(user user) :  this.user = user;
+  UserProfileScreen(User user) :  this.user = user;
 
   @override
   _UserProfileScreenState createState() => _UserProfileScreenState();
@@ -20,7 +21,7 @@ class UserProfileScreen extends StatefulWidget {
 
 class _UserProfileScreenState extends State<UserProfileScreen>{
 
-  user user;
+  User user;
   int id;
 
 
@@ -34,3 +35,94 @@ class _UserProfileScreenState extends State<UserProfileScreen>{
       });
     });
   }
+  
+  Widget _buildAvatar() {
+    return new Hero(
+      tag: "User Profile",
+      child: new CircleAvatar(
+        radius: 50.0,
+      ),
+    );
+  }
+
+  Widget _buildDiagonalImageBackground(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+
+    return new DiagonallyCutColoredImage(
+      new Image.asset(
+        "screens/user/profile_header_background.png",
+        width: screenWidth,
+        height: 280.0,
+        fit: BoxFit.cover,
+      ),
+      color: const Color(0xBB8338f4),
+    );
+  }
+
+  Widget _createPillButton(
+    String text, {
+    Color backgroundColor = Colors.transparent,
+    Color textColor = Colors.white70,
+  }) {
+    return new ClipRRect(
+      borderRadius: new BorderRadius.circular(30.0),
+      child: new MaterialButton(
+        minWidth: 140.0,
+        color: backgroundColor,
+        textColor: textColor,
+        onPressed: () {},
+        child: new Text(text),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
+    var linearGradient = const BoxDecoration(
+      gradient: const LinearGradient(
+        begin: FractionalOffset.centerRight,
+        end: FractionalOffset.bottomLeft,
+        colors: <Color>[
+          const Color(0xFF413070),
+          const Color(0xFF2B264A),
+        ],
+      ),
+    );
+
+    return new Scaffold(
+      body: new SingleChildScrollView(
+        child: new Container(
+          decoration: linearGradient,
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Stack(
+                children: <Widget>[
+                  _buildDiagonalImageBackground(context),
+                  new Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    heightFactor: 1.4,
+                    child: new Column(
+                      children: <Widget>[
+                        _buildAvatar(),
+                        // _buildFollowerInfo(textTheme),
+                        // _buildActionButtons(theme),
+                      ],
+                    ),
+                  ),
+                  new Positioned(
+                    top: 26.0,
+                    left: 4.0,
+                    child: new BackButton(color: Colors.white),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+} 
