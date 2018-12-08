25500000365c3e4923beed26fd0094fd31a0ae32
Hung Nguyen <nthungdev@gmail.com>
2018-12-08 17:44:48 -0500

    setting

---
+++ lib/screens/setting/about.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+  
--- lib/screens/setting/setting.dart
+++ lib/screens/setting/setting.dart
@@ -1,10 +1,15 @@
 
 import 'package:flutter/material.dart';
+import 'package:youroccasions/models/data.dart';
+import 'package:youroccasions/screens/login/login.dart';
 import 'package:youroccasions/screens/setting/setting.dart';
+import 'package:youroccasions/screens/user/upload_avatar.dart';
+import 'package:youroccasions/utilities/config.dart';
+import 'package:youroccasions/screens/setting/about.dart';
+
 class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
@@ -14,80 +19,27 @@ class SettingPage extends StatelessWidget {
         children: <Widget>[
           ListTile (
             title: Text ("Edit Profile Picture"),
+            onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new UploadAvatarPage(Dataset.currentUser.value))),
+        
+          ),
+          ListTile (
+            title: Text ("Suggest Improvements"),
             onTap: () {},
           ),
           ListTile (
-            title: Text ("about"),
-            onTap: () {},
+            title: Text ("About"),
+            onTap:  () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new AboutPage())),
           ),
           ListTile (
-            title: Text ("Suggest Inprovements"),
-            onTap: () {},
+            title: Text ("Logout"),
+            onTap: () async {
+              await logout();
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginWithEmailScreen()));
+            },
           ),
          
-
-
         ],
       ),
-=======
-    var linearGradient = const BoxDecoration(
-      gradient: const LinearGradient(
-        begin: FractionalOffset.centerRight,
-        end: FractionalOffset.bottomLeft,
-        colors: <Color>[
-          Colors.blue,
-          Colors.white,
-        ],
-      ),
-    );
-
-    return Scaffold(appBar: 
-      new AppBar(title: new Text('Setting'),),
-      body: new SafeArea(
-        child: new Container(
-          height: double.infinity,
-          decoration: linearGradient,
-        // padding: const EdgeInsets.only(top: 16.0, bottom: 16.0, left: 12.0, right: 12.0),
-        // child: new Card(
-          child: new Row(
-            children: <Widget>[
-              new Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  new Padding(
-                    padding: const EdgeInsets.only(top: 22.0, bottom: 8.0),
-                    child: new Text("Edit Profile", style: new TextStyle(color: new Color.fromARGB(255, 117, 117, 117), fontSize: 32.0, fontWeight: FontWeight.bold)),
-                  ),
-
-                  new Padding(
-                    padding: const EdgeInsets.only(top: 22.0, bottom: 8.0),
-                    child: new Text("Suggest Improvements", style: new TextStyle(color: new Color.fromARGB(255, 117, 117, 117), fontSize: 32.0, fontWeight: FontWeight.bold)),
-                  ),new Padding(
-                    
-                    padding: const EdgeInsets.only(top: 22.0, bottom: 8.0),
-                    child: new Text("About", style: new TextStyle(color: new Color.fromARGB(255, 117, 117, 117), fontSize: 32.0, fontWeight: FontWeight.bold)),
-                  ),new Padding(
-                    padding: const EdgeInsets.only(top: 22.0, bottom: 8.0),
-                    child: new Text("Log out", style: new TextStyle(color: new Color.fromARGB(255, 117, 117, 117), fontSize: 32.0, fontWeight: FontWeight.bold)),
-                  ),
-
-
-
-                
-                new Divider(),
-                new Text("Youroccasions")
-
-              ],
-            ),
-          ],
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-        )
-      )
-    )
-    
-    // )
->>>>>>> eda4761b9a5d2cff342cdbd7f8a9daad168d5ac8
     );
   }
 }
