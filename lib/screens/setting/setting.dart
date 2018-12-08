796caccbc0db92fd42e6cf508701c9ef0c725ee9
Hung Nguyen <nthungdev@gmail.com>
2018-12-08 17:17:06 -0500

    update setting

--- lib/screens/setting/setting.dart
+++ lib/screens/setting/setting.dart
@@ -1,48 +1,34 @@
 
 import 'package:flutter/material.dart';
+import 'package:youroccasions/screens/setting/setting.dart';
 class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Padding(
-      padding: const EdgeInsets.only(top: 16.0, bottom: 16.0, left: 12.0, right: 12.0),
-      child: new Card(
-        child: new Row(
-          children: <Widget>[
-            new Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                new Padding(
-                  padding: const EdgeInsets.only(top: 22.0, bottom: 8.0),
-                  child: new Text("Edit Profile Picture", style: new TextStyle(color: new Color.fromARGB(255, 117, 117, 117), fontSize: 32.0, fontWeight: FontWeight.bold)),
-                ),
-
-                new Padding(
-                  padding: const EdgeInsets.only(top: 22.0, bottom: 8.0),
-                  child: new Text("Suggest Improvements", style: new TextStyle(color: new Color.fromARGB(255, 117, 117, 117), fontSize: 32.0, fontWeight: FontWeight.bold)),
-                ),new Padding(
-                  
-                  padding: const EdgeInsets.only(top: 22.0, bottom: 8.0),
-                  child: new Text("About", style: new TextStyle(color: new Color.fromARGB(255, 117, 117, 117), fontSize: 32.0, fontWeight: FontWeight.bold)),
-                ),new Padding(
-                  padding: const EdgeInsets.only(top: 22.0, bottom: 8.0),
-                  child: new Text("Log out", style: new TextStyle(color: new Color.fromARGB(255, 117, 117, 117), fontSize: 32.0, fontWeight: FontWeight.bold)),
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          ListTile (
+            title: Text ("Edit Profile Picture"),
+            onTap: () {},
+          ),
+          ListTile (
+            title: Text ("about"),
+            onTap: () {},
+          ),
+          ListTile (
+            title: Text ("Suggest Inprovements"),
+            onTap: () {},
+          ),
+         
 
 
-
-                
-                new Divider(),
-                new Text("Ibrahim Diallo")
-
-              ],
-            ),
-          ],
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-        )
-      )
+        ],
+      ),
     );
   }
 }
-
 
